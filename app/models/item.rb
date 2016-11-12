60286e105a4e158ba3a2555cdd2389d9cefddbfd
som4ik <somar_melhem@hotmail.com>
2016-11-13 02:43:07 +0300

    fix pg

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   include PgSearch
-  pg_search_scope :pg_search, against: [:content],:using => {tsearch: {prefix: true, any_word: true}}
+  pg_search_scope :pg_search, against: [:content],:using => {tsearch: {prefix: true, any_word: true, dictionary: "russian"}}
 
   belongs_to :tender
 end
